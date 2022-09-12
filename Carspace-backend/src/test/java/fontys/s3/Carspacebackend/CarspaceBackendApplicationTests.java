96f297a00011e849b83eee7b4e9b65e7efbc3e8a
Yordan <ydoikov@gmail.com>
2022-09-12 20:16:02 +0200

    Added an example unit test to test pipeline

--- Carspace-backend/src/test/java/fontys/s3/Carspacebackend/CarspaceBackendApplicationTests.java
+++ Carspace-backend/src/test/java/fontys/s3/Carspacebackend/CarspaceBackendApplicationTests.java
@@ -1,13 +1,15 @@
 package fontys.s3.Carspacebackend;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarspaceBackendApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void mockTest() {
+		assertEquals("asd", "asd");
 	}
 
 }
